7d204a11927b94a0532682a0dbb4fe874e9ff15c
scohen-trinity <scohen@trinity.edu>
2024-09-02 17:43:31 -0500

    feat: initial ui changes and status chips

---
+++ public/palette.jsx
@@ -0,0 +1,9 @@
+const palette = {
+    deepSkyBlue: '#30C5FF',
+    gunMetal: '#2A2D34',
+    seaGreen: '#5C946E',
+    cambridgeBlue: '#80C2AF',
+    nonPhotoBlue: '#A0DDE6',
+} 
+
+export default palette
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react';
+
 import './Home.css'
 import users from '../InMemoryData/Users/UserList';
-import { useState } from 'react';
+
+import 'boxicons'
 
 function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
     
-  const removeProfile = () => {
-    moveToNextProfile();
-  };
-
   const matchProfile = () => {
     moveToNextProfile();
   }
+  
+  const removeProfile = () => {
+    moveToNextProfile();
+  };
 
   const moveToNextProfile = () => {
     currentIndex < users.length - 1 ? setCurrentIndex(currentIndex + 1) : setCurrentIndex(0);
@@ -20,19 +23,19 @@ function Home() {
   return (
     <div className='home-wrapper'>
       <div>
-        <h1>Home Page</h1>
+        <h1>Discover Page</h1>
       </div>
       <div className='profile-card'>
-        <h2>{users[currentIndex].name}</h2>
-        <div className='flags'>
-            <p>{users[currentIndex].budget}</p>
-            <p>{users[currentIndex].lifestyle}</p>
-            <p>{users[currentIndex].school}</p>
-            <p>{users[currentIndex].residencyType}</p>
+        <h2 className='name-tag'>{users[currentIndex].name}</h2>
+        <div className='tags'>
+            <p className='tag'>{users[currentIndex].school}</p>
+            <p className='tag'>${users[currentIndex].budget}</p>
+            <p className='tag'>{users[currentIndex].residencyType}</p>
+            <p className='tag'>{users[currentIndex].lifestyle}</p>
         </div>
         <div className='swipe-bar'>
-            <p className='match'><button onClick={removeProfile}>no</button></p>
-            <p className='pass'><button onClick={matchProfile}>yes</button></p>
+            <p><box-icon name='x-circle' onClick={removeProfile} animation='tada-hover'></box-icon></p>
+            <p><box-icon name='check-circle' onClick={matchProfile} animation='tada-hover'></box-icon></p>
         </div>
       </div>
     </div>
